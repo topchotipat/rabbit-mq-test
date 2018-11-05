c2a416e128f5afbe4bed22327cf77908107b840b
Chotipat Poowongthanarat <topz.classic@gmail.com>
2018-11-06 00:48:23 +0700

    fix receive

--- receive.js
+++ receive.js
@@ -1,14 +1,15 @@
+#!/usr/bin/env node
+
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (err, conn) {
-    conn.createChannel(function (err, ch) {
-        var q = 'hello';
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello1';
 
-        ch.assertQueue(q, { durable: false })
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-        ch.consume(q, function (msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, { noAck: true })
-    });
+    ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+  });
 });
